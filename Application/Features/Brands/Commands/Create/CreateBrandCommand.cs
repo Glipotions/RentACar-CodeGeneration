1775263d166ca55c592da6070e14b1c16ac2da7b
Hamza Kavak <69338731+Glipotions@users.noreply.github.com>
2023-08-20 18:00:28 +0300

    first done

---
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,36 @@
+﻿using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Brands.Commands.Create
+{
+    public class CreateBrandCommand : IRequest<CreatedBrandResponse>
+    {
+        public string Name { get; set; }
+
+
+        public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+        {
+            private IBrandRepository _brandRepository;
+            private IMapper _mapper;
+
+            public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+            {
+                _brandRepository = brandRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                Brand brand = _mapper.Map<Brand>(request);
+                brand.Id = Guid.NewGuid();
+
+                await _brandRepository.AddAsync(brand);
+                CreatedBrandResponse response = _mapper.Map<CreatedBrandResponse>(brand);
+
+                return response;
+            }
+        }
+    }
+}
